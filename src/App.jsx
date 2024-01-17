4631fb41f5f78de0cdbfff7b3018c1910a3edc05
Esteban Leandro Marín <elndro85@gmail.com>
2024-01-17 04:04:52 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import './App.css';
+import './index.css'
+import { useState, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+const supabase = createClient('https://yrkqzefmjtupvuljetsy.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlya3F6ZWZtanR1cHZ1bGpldHN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU0MTQ3OTYsImV4cCI6MjAyMDk5MDc5Nn0.I5N4d4oVqnHyC5Pf7ZokJucgZbdbzenKJvcFx9UZog8')
+
+export default function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(async () => {
+    const { data: { session } } = await supabase.auth.getSession()
+    setSession(session)
+
+    const { data: { subscription } } = await supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+  }
+  else {
+    return (<div>Logged in!</div>)
+  }
 }
-
-export default App;

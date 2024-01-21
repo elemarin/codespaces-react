e80fe0adaf33097bebae98afc344020f04d0151a
Esteban Leandro Marín <elndro85@gmail.com>
2024-01-21 15:26:20 +0000

    removed a test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,8 +2,8 @@ import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeDefined();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeDefined();
+// });
